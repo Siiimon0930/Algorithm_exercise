ac73e0c2ee9d8d45a10880f834e71cb93090dec0
Siiimon <756731959@qq.com>
2023-01-10 17:14:36 +0800

    字符串：leetcode28. 找出字符串中第一个匹配项的下标

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import codeCapriccio.array.Array;
 import codeCapriccio.hashTable.ThreeSum;
 import codeCapriccio.linkedList.MyLinkedList;
+import codeCapriccio.string.StrStr;
 
-public class Main implements ThreeSum {
+public class Main implements ThreeSum, StrStr {
     public static void main(String[] args){
-        StringBuilder res = new StringBuilder();
-        res.insert(0, "123");
-        res.insert(0, "222 ");
-        System.out.println(res);
+        String s = "12345";
+        String a = "12345";
+        System.out.println(s.substring(0, 3));
+        int res = StrStr.strStr("abc", "c");
     }
 }
