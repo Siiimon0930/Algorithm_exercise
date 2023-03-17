af91821496793e8e83e8392f5f6ce7b88c268c35
Siiimon <756731959@qq.com>
2023-03-17 09:50:15 +0800

    贪心：leetcode455. 分发饼干

---
+++ src/codeCapriccio/greedy/FindContentChildren.java
@@ -0,0 +1,46 @@
+package codeCapriccio.greedy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FindContentChildren {
+    /*
+    * 题意（leetcode455. 分发饼干）：
+    *   假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+    *   对每个孩子 i，都有一个胃口值g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j]。
+    *   如果 s[j]>= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+    * 解题思路：
+    */
+
+    // 思路1：优先考虑饼干，小饼干先喂饱小胃口
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start = 0;
+        int count = 0;
+        for (int i = 0; i < s.length && start < g.length; i++) {
+            if (s[i] >= g[start]) {
+                start++;
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    // 思路2：优先考虑胃口，先喂饱大胃口
+    public int findContentChildren2(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int start = s.length - 1;
+        // 遍历胃口
+        for (int index = g.length - 1; index >= 0; index--) {
+            if(start >= 0 && g[index] <= s[start]) {
+                start--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
