ee6e2d453db76c0f0eb3cf9b445537b5160c3860
Siiimon2324 <Siiimon2324@gmail.com>
2023-07-14 19:46:13 +0800

    二刷：leetcode704.数组二分查找

---
+++ src/codeCapriccio/array/BiSearch.java
@@ -0,0 +1,25 @@
+package codeCapriccio.array;
+
+/*
+* 题意（leetcode）704. 二分查找：给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，
+* 写一个函数搜索nums中的 target，如果目标值存在返回下标，否则返回 -1。
+* 解题思路：二分查找
+*/
+public class BiSearch {
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int mid;
+        while(left <= right){
+            mid = (left + right) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
