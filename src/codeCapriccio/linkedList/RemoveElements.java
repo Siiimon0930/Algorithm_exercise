4663bc52dd80a8068c6b46b627820745bb2f6f5b
Siiimon0930 <756731959@qq.com>
2023-07-15 18:54:35 +0800

    链表（二刷）：leetcode203. 移除链表元素*注意设置一个虚拟头节点会方便很多

--- src/codeCapriccio/linkedList/RemoveElements.java
+++ src/codeCapriccio/linkedList/RemoveElements.java
@@ -1,71 +1,35 @@
 package codeCapriccio.linkedList;
 
-public interface RemoveElements {
-    /*
-    * 题目描述（leetcode203）：给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
-    * 解题思路：用两个指针去删除，一个pre，一个current
-    * 时间复杂度：O(n)
-    * */
-//    default ListNode removeElements(ListNode head, int val){
-//        if(head == null)
-//            throw new NullPointerException("不要传一个空链表进来！");
-//
-//        // 有头节点版本
-////        ListNode pre = head;
-////        ListNode current = head.next;
-////
-////        while(current != null){
-////            if(current.val == val){
-////                pre.next = current.next;
-////                current = current.next;
-////            }else{
-////                pre = current;
-////                current = current.next;
-////            }
-////        }
-//        // 无头节点版本
-//        if(head == null || (head.next == null && head.val == val))
-//            return null;
-//
-//        ListNode pre = head;
-//        ListNode current = head;
-//
-//        while(current != null){
-//            if(current.val == val){
-//                pre.next = current.next;
-//                current = current.next;
-//            }else{
-//                pre = current;
-//                current = current.next;
-//            }
-//        }
-//
-//        // 最后判断第一个节点是否要移除
-//        if(head.val == val)
-//            head = head.next;
-//
-//        return head;
-//    }
-    /**
-     * 添加虚节点方式
-     * 时间复杂度 O(n)
-     * 空间复杂度 O(1)
-     * @param head
-     * @param val
-     * @return
-     */
-    default ListNode removeElements(ListNode head, int val) {
-        if (head == null) {
-            return head;
+/*
+* 题意（leetcode203. 移除链表元素）：
+* 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+* 解题思路：主要是要注意有没有头节点
+*/
+public class RemoveElements {
+    public ListNode removeElements(ListNode head, int val) {
+        // 边界条件
+        if(head == null || (head.next == null && head.val == val))
+            return null;
+        ListNode front = head, cur = head.next;
+        while(cur != null){
+            if(cur.val == val){
+                front.next = cur.next;
+            }else {
+                front = cur;
+            }
+            cur = cur.next;
         }
-        // 因为删除可能涉及到头节点，所以设置dummy节点，统一操作
+        return head.val == val ? head.next : head;
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+        // 添加虚拟头节点
         ListNode dummy = new ListNode(-1, head);
-        ListNode pre = dummy;
-        ListNode cur = head;
-        while (cur != null) {
-            if (cur.val == val) {
+        ListNode pre = dummy, cur = head;
+        while(cur != null){
+            if(cur.val == val){
                 pre.next = cur.next;
-            } else {
+            }else {
                 pre = cur;
             }
             cur = cur.next;
