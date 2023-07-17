b4e2c30a06122fab75cd92c59d2010a1b2554808
Siiimon0930 <756731959@qq.com>
2023-07-17 21:54:46 +0800

    哈希表（二刷）：leetcode242. 有效的字母异位词

--- src/codeCapriccio/hashTable/IsAnagram.java
+++ src/codeCapriccio/hashTable/IsAnagram.java
@@ -1,38 +1,31 @@
 package codeCapriccio.hashTable;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
-public interface IsAnagram {
-    /*
-    * 题目描述（leetcode242）：给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
-    * 解题思路：用一个哈希表计算第一个字符串s每个字母出现的频率，而后比对t的字母频率
-    * 时间复杂度：O（n）
-    * */
-    // 数组法
-//    default boolean isAnagram(String s, String t) {
-//        int[] hash = new int[26];
-//        for(int i = 0; i < s.length(); i++)
-//            hash[s.charAt(i) - 'a']++;
-//        for(int i = 0; i < t.length(); i++)
-//            hash[t.charAt(i) - 'a']--;
-//        for(int i = 0; i < 26; i++){
-//            if(hash[i] != 0)
-//                return false;
-//        }
-//        return true;
-//    }
-    // hashMap法
-    default boolean isAnagram(String s, String t) {
-        Map<Character, Integer> hash = new HashMap<>();
-        for(int i = 0; i < s.length(); i++)
-            hash.put(s.charAt(i), hash.getOrDefault(s.charAt(i), 0) + 1);
-        for(int i = 0; i < t.length(); i++)
-            hash.put(t.charAt(i), hash.getOrDefault(t.charAt(i), 0) - 1);
-        for(int i : hash.values()){
-            if(i != 0)
+/*
+* leetcode242. 有效的字母异位词:
+* 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+* 注意：若s 和 t中每个字符出现的次数都相同，则称s 和 t互为字母异位词。
+* 解题思路：设置一个hashmap记录字母的出现次数，其中遍历s时增加次数，遍历t时减少，最终看hashmap中是不是都是0；（设定一个数组来存储频率也是可以的）
+*/
+public class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        // 1.遍历s，频次+
+        for (char c : s.toCharArray()) {
+            hashMap.put(c, hashMap.getOrDefault(c, 0)+1);
+        }
+        // 2.遍历t，频次-
+        for (char c : t.toCharArray()) {
+            hashMap.put(c, hashMap.getOrDefault(c, 0)-1);
+        }
+        // 3.遍历hashmap，value都为0才符合条件
+        for (Integer value : hashMap.values()) {
+            if(value != 0)
                 return false;
         }
+
         return true;
     }
 }
