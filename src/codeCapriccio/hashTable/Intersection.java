d5a3c487f78cbe9724b459f5ad07184be52aa8e4
Siiimon <756731959@qq.com>
2023-07-17 22:22:08 +0800

    哈希表（二刷）：leetcode349. 两个数组的交集*把stream流操作练熟了！

--- src/codeCapriccio/hashTable/Intersection.java
+++ src/codeCapriccio/hashTable/Intersection.java
@@ -1,32 +1,28 @@
 package codeCapriccio.hashTable;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
 
-public interface Intersection {
-    /*
-    * 题目描述（leetcode349）：给定两个数组 nums1 和 nums2 ，返回 它们的交集 。
-    * 输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
-    * 解题思路：用两个集合set1，set2记录两个数组出现的数字，而后两个集合的交集就是两个数组的交集
-    * 时间复杂度：O（n）空间复杂度：O（n）
-    * */
-    default int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        Set<Integer> result = new HashSet<>();
-        for(int i = 0; i < nums1.length; i++)
-            set1.add(nums1[i]);
-        for(int i = 0; i < nums2.length; i++)
-            set2.add(nums2[i]);
-        result.addAll(set1);
-        result.retainAll(set2);
-        int length = result.toArray().length;
-        int[] re = new int[length];
-        Object[] re2 = result.toArray();
-        for(int i = 0;i < length; i++){
-            re[i] = (int)re2[i];
+/*
+* leetcode349. 两个数组的交集:
+* 给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+* 解题思路：用一个hashset记录num1中出现的数字，遍历num2时去hashset中查
+*/
+public class Intersection {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> temp = new HashSet<>();
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        // 1.遍历num1
+        for (int i : nums1) {
+            hashSet.add(i);
         }
-        return re;
+        // 2.遍历num2
+        for (int i : nums2) {
+            if(hashSet.contains(i))
+                temp.add(i);
+        }
+        // 把hashset转为int数组
+        int[] result = temp.stream().mapToInt(Integer::intValue).toArray();
+        return result;
     }
 }
