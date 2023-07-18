e94a006db96ae1a5b888b36fd2027cefd500cb9b
Siiimon <756731959@qq.com>
2023-07-18 10:12:56 +0800

    哈希表（二刷）：leetcode202. 快乐数*想不到判断条件！

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,14 @@ import codeCapriccio.DP.CombinationSum4;
 import codeCapriccio.greedy.LemonadeChange;
 import codeCapriccio.greedy.MonotoneIncreasingDigits;
 import codeCapriccio.greedy.WeeklyGame337;
+import codeCapriccio.hashTable.IsHappy;
 import codeCapriccio.linkedList.ListNode;
 import codeCapriccio.linkedList.MyLinkedList;
 import codeCapriccio.stackAndQueue.IsValid;
 
 public class Main implements IsValid {
     public static void main(String[] args){
-        MyLinkedList myLinkedList = new MyLinkedList(new ListNode(1));
-        myLinkedList.addAtTail(3);
-        myLinkedList.addAtIndex(1, 2);
-        myLinkedList.get(1);
-        myLinkedList.deleteAtIndex(1);
-        myLinkedList.get(1);
+        IsHappy isHappy = new IsHappy();
+        boolean happy = isHappy.isHappy(19);
     }
 }
--- src/codeCapriccio/hashTable/IsHappy.java
+++ src/codeCapriccio/hashTable/IsHappy.java
@@ -3,27 +3,32 @@ package codeCapriccio.hashTable;
 import java.util.HashSet;
 import java.util.Set;
 
-public interface IsHappy {
-    /*
-    * 题目描述（leetcode202）：编写一个算法来判断一个数 n 是不是快乐数。
-    * 解题思路：使用哈希法，来判断这个sum是否重复出现，如果重复了就是return false， 否则一直找到sum为1为止。
-    * */
-    default boolean isHappy(int n) {
-        Set<Integer> record = new HashSet<>();
-        while (n != 1 && !record.contains(n)) {
-            record.add(n);
-            n = getNextNumber(n);
+/*
+* leetcode202. 快乐数:
+* 编写一个算法来判断一个数 n 是不是快乐数。「快乐数」定义为：
+*   对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+*   然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+*   如果这个过程 结果为1，那么这个数就是快乐数。
+* 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+* 解题思路：无限循环意味着 sum值会重复出现，使用set保存每次的sum，如果重复了则return false
+*/
+public class IsHappy {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        // 123
+        int sum = 0;
+        while(n != 1){
+            while(n >= 1){
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            // 如果set中已出现过相同sum，return false
+            if(set.contains(sum))
+                return false;
+            set.add(sum);
+            n = sum;
+            sum = 0;
         }
-        return n == 1;
-    }
-
-    default int getNextNumber(int n) {
-        int res = 0;
-        while (n > 0) {
-            int temp = n % 10;
-            res += temp * temp;
-            n = n / 10;
-        }
-        return res;
+        return true;
     }
 }
