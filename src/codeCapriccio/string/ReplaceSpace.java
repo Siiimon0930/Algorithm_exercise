cc628ceb63b198703e70f4b81f733b2e8a2eb850
Siiimon <756731959@qq.com>
2023-07-25 10:40:07 +0800

    字符串（二刷）：剑指 Offer 05. 替换空格

--- src/codeCapriccio/string/ReplaceSpace.java
+++ src/codeCapriccio/string/ReplaceSpace.java
@@ -1,19 +1,18 @@
 package codeCapriccio.string;
 
-public interface ReplaceSpace {
-    /*
-    * 题目描述（剑指 Offer 05. 替换空格）：请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
-    * 解题思路，就遍历然后替换呀
-    * */
-    default String replaceSpace(String s) {
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == ' '){
-                res.append("%20");
-            }else {
-                res.append(s.charAt(i));
-            }
+/*
+* leetcode剑指 Offer 05. 替换空格:请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+* 解题思路：遍历就完事了
+*/
+public class ReplaceSpace {
+    public String replaceSpace(String s) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (char c : s.toCharArray()) {
+            if(c != ' ')
+                stringBuffer.append(c);
+            else
+                stringBuffer.append("%20");
         }
-        return res.toString();
+        return stringBuffer.toString();
     }
 }
