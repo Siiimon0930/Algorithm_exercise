f776b4e5e74fb2896fbb38fa6dfb1654e79c716a
Siiimon <756731959@qq.com>
2023-07-27 21:23:26 +0800

    栈和队列（二刷）：leetcode1047. 删除字符串中的所有相邻重复项

--- src/codeCapriccio/stackAndQueue/RemoveDuplicates.java
+++ src/codeCapriccio/stackAndQueue/RemoveDuplicates.java
@@ -1,22 +1,28 @@
 package codeCapriccio.stackAndQueue;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
-public interface RemoveDuplicates {
-    /*
-    * 题目描述（leetcode1047. 删除字符串中的所有相邻重复项:）输入："abbaca" 输出："ca"
-    * 解题思路：遍历一遍字符串，与栈顶元素不同则push，相同则pop，最终栈剩余的元素就是答案字符串
-    * 时间复杂度：O（n）
-    * */
-    default String removeDuplicates(String s) {
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(res.length() == 0 || res.charAt(res.length() - 1) != s.charAt(i))
-                res.append(s.charAt(i));
-            else
-                res.deleteCharAt(res.length() - 1);
+/*
+* leetcode 1047. 删除字符串中的所有相邻重复项:
+*   给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+*   在 S 上反复执行重复项删除操作，直到无法继续删除。
+*   在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+* 解题思路：栈解决
+*/
+public class RemoveDuplicates {
+    public String removeDuplicates(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if(stack.isEmpty() || stack.peek() != c){
+                stack.push(c);
+            }else
+                stack.pop();
         }
-        return res.toString();
+        while(!stack.isEmpty()){
+            stringBuilder.append(stack.pop());
+        }
+        return stringBuilder.reverse().toString();
     }
 }
