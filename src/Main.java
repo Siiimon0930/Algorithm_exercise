c6208f3a87423ba5d5631bc87064f057c1737af5
Siiimon <756731959@qq.com>
2023-07-27 21:09:37 +0800

    栈和队列（二刷）：leetcode20. 有效的括号

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-import codeCapriccio.DP.Change;
-import codeCapriccio.DP.CombinationSum4;
-import codeCapriccio.greedy.LemonadeChange;
-import codeCapriccio.greedy.MonotoneIncreasingDigits;
-import codeCapriccio.greedy.WeeklyGame337;
-import codeCapriccio.hashTable.IsHappy;
-import codeCapriccio.linkedList.ListNode;
-import codeCapriccio.linkedList.MyLinkedList;
 import codeCapriccio.stackAndQueue.IsValid;
-import codeCapriccio.string.ReverseStr;
 import codeCapriccio.string.ReverseWords;
 
-public class Main implements IsValid {
+public class Main {
     public static void main(String[] args){
         String s = "the sky is blue";
         ReverseWords reverseWords = new ReverseWords();
--- src/codeCapriccio/stackAndQueue/IsValid.java
+++ src/codeCapriccio/stackAndQueue/IsValid.java
@@ -1,57 +1,39 @@
 package codeCapriccio.stackAndQueue;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Stack;
 
-public interface IsValid {
-    /*
-    * 题目描述（leetcode20. 有效的括号）：给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
-    * 解题思路：左括号入栈，右括号pop栈，若pop的和右括号不匹配则失败，中间不失败且最终栈是空的才成功
-    * 时间复杂度：O（n）
-    * */
-    static boolean isValid(String s) {
-        if(s.length() % 2 != 0)
-            return false;
-        char[] stack = new char[s.length()];
-        int length = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
-                stack[length++] = s.charAt(i);
-            }else {
-                if(length < 1)
+/*
+* leetcode20. 有效的括号:
+*   给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+*   有效字符串需满足：
+*       左括号必须用相同类型的右括号闭合
+*       左括号必须以正确的顺序闭合。
+*       每个右括号都有一个对应的相同类型的左括号。
+* 解题思路：一个栈解决，左括号push右pop
+*/
+public class IsValid {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if(c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else {
+                if(stack.isEmpty())
+                    // 如果遇到右括号栈为空则直接return false
                     return false;
-                char popValue = stack[--length];
-                if(s.charAt(i) == ')' && popValue != '(')
+                char cc = stack.pop();
+                // 括号不匹配返回false
+                if(c == ')' && cc != '(')
                     return false;
-                if(s.charAt(i) == ']' && popValue != '[')
+                else if (c == ']' && cc != '[') {
                     return false;
-                if(s.charAt(i) == '}' && popValue != '{')
+                } else if (c == '}' && cc != '{') {
                     return false;
+                }
             }
         }
-
-        return length == 0;
+        // 遍历完后不可以有多余的左括号还在栈内
+        return stack.isEmpty();
     }
-    // 更巧妙的小trick：在push的时候先判断是什么左括号，然后push对应的右括号进去，而后在碰到右括号时就直接比对栈顶元素是否与当前元素相等即可，无需三个if了。
-//    public boolean isValid(String s) {
-//        Deque<Character> deque = new LinkedList<>();
-//        char ch;
-//        for (int i = 0; i < s.length(); i++) {
-//            ch = s.charAt(i);
-//            //碰到左括号，就把相应的右括号入栈
-//            if (ch == '(') {
-//                deque.push(')');
-//            }else if (ch == '{') {
-//                deque.push('}');
-//            }else if (ch == '[') {
-//                deque.push(']');
-//            } else if (deque.isEmpty() || deque.peek() != ch) {
-//                return false;
-//            }else {//如果是右括号判断是否和栈顶元素匹配
-//                deque.pop();
-//            }
-//        }
-//        //最后判断栈中元素是否匹配
-//        return deque.isEmpty();
-//    }
 }
