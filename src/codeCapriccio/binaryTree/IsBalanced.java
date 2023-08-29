351de77421fd452b85667a9f5d5481f76e6df465
Siiimon <756731959@qq.com>
2023-08-29 18:05:32 +0800

    二叉树（二刷）：leetcode110. 平衡二叉树

--- src/codeCapriccio/binaryTree/IsBalanced.java
+++ src/codeCapriccio/binaryTree/IsBalanced.java
@@ -1,30 +1,29 @@
 package codeCapriccio.binaryTree;
 
-public interface IsBalanced {
-    /*
-    * 题意（110. 平衡二叉树）：一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1
-    * 解题思路：
-    *   1. 递归法（后序思想）：递归求左右子树的高度
-    *
-    * */
-
-
-    default boolean isBalanced(TreeNode root) {
+/*
+* leetcode110. 平衡二叉树:给定一个二叉树，判断它是否是高度平衡的二叉树。
+* 解题思路：从底向上判断
+*/
+public class IsBalanced {
+    public boolean isBalanced(TreeNode root) {
         if(root == null)
             return true;
-        else if (!isBalanced(root.left) || !isBalanced(root.right)) {   // 先判断左右子树，即左右中
+
+        int left = getMaxDepth(root.left);
+        int right = getMaxDepth(root.right);
+        // 先判断当前节点的左右子树高度差是否满足要求
+        if(Math.abs(left - right) > 1)
             return false;
-        } else{
-            int leftDepth = getDepth(root.left);
-            int rightDepth = getDepth(root.right);
-            return Math.abs(leftDepth - rightDepth) <= 1;
-        }
+        // 左右子树高度差是否满足要求的前提下再取决于左右树是否也同时为平衡二叉树
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 
-    default int getDepth(TreeNode root){
+    public int getMaxDepth(TreeNode root){
         if(root == null)
             return 0;
-        else
-            return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+        int left = getMaxDepth(root.left);
+        int right = getMaxDepth(root.right);
+
+        return Math.max(left, right) + 1;
     }
 }
