584c7c411bf7fdf0902e4e1fd107408de21b7828
Siiimon <756731959@qq.com>
2023-08-29 13:18:38 +0800

    二叉树（二刷）：leetcode226. 翻转二叉树

--- src/codeCapriccio/binaryTree/InvertTree.java
+++ src/codeCapriccio/binaryTree/InvertTree.java
@@ -1,45 +1,18 @@
 package codeCapriccio.binaryTree;
 
-import java.util.Stack;
-
-public interface InvertTree {
-    /*
-    * 题意（leetcode226）：翻转二叉树
-    * 解题思路：
-    *   1. 递归法
-    *   2. 迭代法
-    * */
-
-    // 1. 递归法
-    default TreeNode invertTree(TreeNode root) {
+/*
+* leetcode226. 翻转二叉树:给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+* 解题思路：后序递归
+*/
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
         if(root == null)
-            return null;
-        else{
-            TreeNode temp = root.left;
-            root.left = root.right;
-            root.right = temp;
-            invertTree(root.left);
-            invertTree(root.right);
             return root;
-        }
-    }
-
-    // 2. 迭代法，可以使用迭代先序遍历来模拟
-    default TreeNode invertTree2(TreeNode root) {
-        if(root == null)
-            return null;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while(!stack.empty()){
-            TreeNode tempNode = stack.pop();
-            TreeNode temp = tempNode.left;
-            tempNode.left = tempNode.right;
-            tempNode.right = temp;
-            if(tempNode.left != null)
-                stack.push(tempNode.left);
-            if(tempNode.right != null)
-                stack.push(tempNode.right);
-        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
         return root;
     }
+
 }
