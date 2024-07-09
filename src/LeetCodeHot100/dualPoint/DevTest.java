9f176d7d92c02b25ecd66e37b42cbde94df95c32
Siiimon <756731959@qq.com>
2024-07-09 22:22:15 +0800

    Dev 分支push

--- src/LeetCodeHot100/dualPoint/DevTest.java
+++ src/LeetCodeHot100/dualPoint/DevTest.java
@@ -6,4 +6,7 @@ package LeetCodeHot100.dualPoint;
  * @Date: 2024/07/09/22:18
  */
 public class DevTest {
+    public static void main(String[] args) {
+        System.out.println("Dev 分支");
+    }
 }
