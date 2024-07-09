657ecdddfb378080f446dff33cf586b233ae5faa
Siiimon <756731959@qq.com>
2024-07-09 21:43:42 +0800

    力扣hot100 双指针 - 42.接雨水 *** 乱写测试debug

---
+++ src/LeetCodeHot100/dualPoint/Trap.java
@@ -0,0 +1,23 @@
+package LeetCodeHot100.dualPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: 42.接雨水（抄答案）
+ * @Author: Siiimon
+ * @Date: 2024/07/09/12:22
+ */
+public class Trap {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.forEach(e -> {
+            System.out.println(e);
+        });
+    }
+}
